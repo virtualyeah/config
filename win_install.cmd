e5fb061aa96d0bd0897673a96c661807a5ed543f
virtual <vvirtual311@gmail.com>
2022-12-04 20:43:43 -0800

    Make install script not prompt user for overwrites

--- win_install.cmd
+++ win_install.cmd
@@ -2,7 +2,7 @@
 
 :: Copy all config files to the correct locations.
 echo Current: helix-editor
-xcopy helix\config.toml %UserProfile%\Appdata\Roaming\helix\ & cls
+xcopy /y helix\config.toml %UserProfile%\Appdata\Roaming\helix\ & cls
 
 :: Exit message.
 cls
